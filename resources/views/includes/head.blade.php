47cf856c9da722dd957e70182ced2e1e9a3fd424
Miroslav Zíka <67856116+zikam-KVD@users.noreply.github.com>
2023-12-09 17:00:29 +0100

    příprava struktury

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Pokemons</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</head>
